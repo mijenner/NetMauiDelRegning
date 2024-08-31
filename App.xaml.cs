68ab9f8978af86eafd0c2f1843e5e40a3f1b71ca
mijenner <mijenner@gmail.com>
2024-08-31 10:06:50 +0200

    Added .NET MAUI project (and solution in same folder).

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NetMauiDelRegning
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NetMauiDelRegning
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
